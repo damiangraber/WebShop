7e2169ff50b469f74fab6f6d565e71e0e41fc3d3
damiangraber <damian.graver@gmail.com>
2016-11-06 11:59:50 +0000

    struktura bazy danych i aktualizacja klasy Item

---
+++ database/WebShop.sql
@@ -0,0 +1,82 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.10deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Czas wygenerowania: 06 Lis 2016, 11:56
+-- Wersja serwera: 5.5.49-0ubuntu0.14.04.1
+-- Wersja PHP: 5.5.9-1ubuntu4.17
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Baza danych: `WebShop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `Categories`
+--
+
+CREATE TABLE IF NOT EXISTS `Categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(150) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `Items`
+--
+
+CREATE TABLE IF NOT EXISTS `Items` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(150) NOT NULL,
+  `price` decimal(10,0) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `quantity_on_stock` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `category_id` (`category_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `Users`
+--
+
+CREATE TABLE IF NOT EXISTS `Users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `fname` varchar(100) NOT NULL,
+  `lname` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `hashed_password` varchar(255) NOT NULL,
+  `shipping_address` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- Ograniczenia dla zrzutów tabel
+--
+
+--
+-- Ograniczenia dla tabeli `Items`
+--
+ALTER TABLE `Items`
+  ADD CONSTRAINT `Items_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `Categories` (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- src/Item.php
+++ src/Item.php
@@ -1,1 +1,83 @@
 <?php
+
+
+class Item {
+
+    private $id;
+    private $name;
+    private $price;
+    private $category;
+    private $description;
+    private $quantityOnStock;
+
+    public function __construct() {
+        $this->id = - 1;
+        $this->name = '';
+        $this->price = 0;
+        $this->category = 0;
+        $this->description = '';
+        $this->quantityOnStock = 0;
+    }
+
+
+    public function getId() {
+        return $this->id;
+    }
+
+
+    public function getName() {
+        return $this->name;
+    }
+
+
+    public function setName($name) {
+        if (is_string($name) && strlen(trim($name)) > 0) {
+            $this->name = trim($name);
+        }
+    }
+
+    public function getPrice() {
+        return $this->price;
+    }
+
+    public function setPrice($price) {
+        if (is_numeric($price) && $price >= 0) {
+            $this->price = trim($price);
+        }
+    }
+
+    public function getCategory() {
+        return $this->category;
+    }
+
+
+    public function setCategory($category) {
+        if (is_int($category) && $category >= 0) {
+            $this->category = trim($category);
+        }
+    }
+
+    public function getDescription() {
+        return $this->description;
+    }
+
+
+    public function setDescription($description) {
+        if (is_string($description) && strlen(trim($description)) > 0) {
+            $this->description = trim($description);
+        }
+    }
+
+
+    public function getQuantityOnStock() {
+        return $this->quantityOnStock;
+    }
+
+
+    public function setQuantityOnStock($quantityOnStock) {
+        if (is_int($quantityOnStock) && $quantityOnStock >= 0) {
+            $this->quantityOnStock = trim($quantityOnStock);
+        }
+    }
+
+}
